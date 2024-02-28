226f922be8a487dae93bc96b6d5c188cc76918b8
gitkadam <102312568+gitkadam@users.noreply.github.com>
2024-02-28 16:48:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -42,22 +42,6 @@ resource "aws_instance" "example" {
     Name = "terraform-example"
   }
 
-#   provisioner "remote-exec" {
-#     inline = [
-#       "sudo yum update -y",
-#       "sudo yum install -y docker",
-#       "sudo service docker start",
-#       "sudo usermod -aG docker ec2-user"
-#     ]
-
-#     connection {
-#       type        = "ssh"
-#       user        = "ec2-user"
-#       private_key = file("path_to_your_private_key.pem")
-#       host        = self.public_ip
-#     }
-#   }
-}
 
 # Create Security Group
 resource "aws_security_group" "instance_sg" {
