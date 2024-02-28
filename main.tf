798c074c6590d24e388479a82afb8a769fe96189
gitkadam <102312568+gitkadam@users.noreply.github.com>
2024-02-28 16:47:19 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,94 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+resource "aws_subnet" "public1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.4.0/24"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.rt.id
+}
+
+
+#create a ec2 instance
+resource "aws_instance" "example" {
+  ami           = "ami-0440d3b780d96b29d"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public.id
+
+  tags = {
+    Name = "terraform-example"
+  }
+
+#   provisioner "remote-exec" {
+#     inline = [
+#       "sudo yum update -y",
+#       "sudo yum install -y docker",
+#       "sudo service docker start",
+#       "sudo usermod -aG docker ec2-user"
+#     ]
+
+#     connection {
+#       type        = "ssh"
+#       user        = "ec2-user"
+#       private_key = file("path_to_your_private_key.pem")
+#       host        = self.public_ip
+#     }
+#   }
+}
+
+# Create Security Group
+resource "aws_security_group" "instance_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# create load application load balancer
+resource "aws_lb" "test" {
+  name               = "test-lb-tf"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.instance_sg.id]
+  subnets            = [aws_subnet.public.id, aws_subnet.public1.id ]
+
+  enable_deletion_protection = true
+
+  tags = {
+    Environment = "test"
+  }
+}
